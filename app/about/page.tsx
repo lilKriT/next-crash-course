7e586f19c3157cfb2bd5c598ce47575e6b881f49
Piotr Filipiak <kiddhkane@gmail.com>
2023-06-01 23:20:40 +0200

    Added a simple nested route.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-const About = () => {
+const AboutPage = () => {
   return (
     <div>
-      <head>
-        <title>About</title>
-      </head>
-      About me
+      <h2>About lilKriT</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quo
+        earum, consectetur harum deleniti recusandae voluptatum! Nisi quis qui
+        delectus rerum sequi laborum officia alias illo nostrum. In temporibus
+        sequi, eveniet veritatis, beatae, quo totam voluptate voluptas fugiat
+        incidunt soluta!
+      </p>
     </div>
   );
 };
 
-export default About;
+export default AboutPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import Link from "next/link";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>Welcome to lilKriT's Page</h1>
+      <ul>
+        <li>
+          <Link href={`/`}>Home</Link>
+        </li>
+        <li>
+          <Link href={`/about`}>About</Link>
+        </li>
+        <li>
+          <Link href={`/about/team`}>Team</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default HomePage;
