6eceb64da35a63b6cb3274e75ae33e8ed62ec2f4
Piotr Filipiak <kiddhkane@gmail.com>
2023-06-03 01:03:02 +0200

    Displaying repo dirs. With a delay (on purpose).

--- app/code/repos/[name]/page.tsx
+++ app/code/repos/[name]/page.tsx
@@ -1,5 +1,6 @@
-import Repo from "@/app/components/Repo";
 import Link from "next/link";
+import Repo from "@/app/components/Repo";
+import RepoDirs from "@/app/components/RepoDirs";
 
 // This is a bit weird, but it works ;)
 // First I destructure params to get name, then I declare the type as a literal.
@@ -10,6 +11,8 @@ const RepoPage = ({ params: { name } }: { params: { name: string } }) => {
       {/* This error is hardcoded in TS! */}
       {/* @ts-expect-error Server Component */}
       <Repo name={name} />
+      {/* @ts-expect-error Server Component */}
+      <RepoDirs name={name} />
     </div>
   );
 };
---
+++ app/components/RepoDirs.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { Key } from "react";
+
+const fetchRepoContents = async (name: string) => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  const response = await fetch(
+    `https://api.github.com/repos/lilKriT/${name}/contents`
+  );
+  const contents = await response.json();
+
+  return contents;
+};
+
+const RepoDirs = async ({ name }: { name: string }) => {
+  const contents = await fetchRepoContents(name);
+  const dirs = contents.filter(
+    (content: { type: string }) => content.type === "dir"
+  );
+
+  return (
+    <div>
+      <h3>Directories</h3>
+      <ul>
+        {dirs.map((dir: { path: Key | null | undefined }) => (
+          <li key={dir.path}>
+            <Link href={`/code/repos/${name}/${dir.path}`}>{dir.path}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RepoDirs;
