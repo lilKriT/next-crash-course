bd19bf5a21fd4066a39964561465621c71107b10
Piotr Filipiak <kiddhkane@gmail.com>
2023-06-03 00:10:18 +0200

    Putting params into single pages.

--- app/code/repos/[name]/page.tsx
+++ app/code/repos/[name]/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const RepoPage = () => {
-  return <div>RepoPage</div>;
+// interface IRepoPage {
+//   params: {
+//     name: string;
+//   };
+// }
+
+// This is a bit weird, but it works ;)
+// First I destructure params to get name, then I declare the type as a literal.
+const RepoPage = ({ params: { name } }: { params: { name: string } }) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>Repo details</p>
+    </div>
+  );
 };
 
 export default RepoPage;
