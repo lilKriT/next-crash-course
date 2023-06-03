8aa8327ba9b8baab16c395f124c310a467e85888
Piotr Filipiak <kiddhkane@gmail.com>
2023-06-03 03:29:35 +0200

    Refactored displaying courses to client.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,32 @@
+"use client"; // we need this because search from will need useEffect and useState
 import Link from "next/link";
 import Courses from "./components/Courses";
+import { useState, useEffect } from "react";
+import LoadingPage from "./loading"; // we need to add this manually because this is a client component
 
 const HomePage = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch("/api/courses");
+      const data = await res.json();
+
+      setCourses(data);
+      setLoading(false);
+    };
+    fetchCourses();
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
   return (
     <div>
       <h1>Welcome to lilKriT's Page</h1>
-      {/* @ts-expect-error Server Component */}
-      <Courses />
+      <Courses courses={courses} />
     </div>
   );
 };
