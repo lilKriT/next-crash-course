7a05e93e78e6a608078f374b37ee4814fb21e8c7
Piotr Filipiak <kiddhkane@gmail.com>
2023-06-03 03:11:57 +0200

    Adding courses (temporarily).

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,7 +1,31 @@
 import courses from "./data.json";
+import { v4 as uuidv4 } from "uuid";
 import { NextResponse, NextRequest } from "next/server";
 
-export async function GET(request: NextRequest) {
+export const GET = async (request: NextRequest) => {
   // return NextResponse(courses); // this is just text
   return NextResponse.json(courses); // this is json
-}
+};
+
+export const POST = async (request: NextRequest) => {
+  const {
+    title,
+    description,
+    level,
+    link,
+  }: { title: string; description: string; level: string; link: string } =
+    await request.json();
+  // console.log(title, description, level, link);
+
+  const newCourse = {
+    id: parseInt(uuidv4()),
+    title,
+    description,
+    level,
+    link,
+  };
+
+  courses.push(newCourse);
+
+  return NextResponse.json(courses);
+};
